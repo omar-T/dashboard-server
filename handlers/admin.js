55b6f94cd421f638a819a25beae7265174215080
xotx <ot.themechanic95@gmail.com>
2019-12-10 23:03:23 +0300

    Refactor update method for admin.

--- handlers/admin.js
+++ handlers/admin.js
@@ -39,7 +39,25 @@ exports.deleteAdmin = async function(req, res, next){
 // UPDATE - /api/admins/:admin_id
 exports.updateAdmin = async function(req, res, next){
     try{
-        let updatedAdmin = await db.Admin.findOneAndUpdate({_id: req.params.admin_id}, req.body, {new: true});
+        let adminData = req.body;
+        let decode = Buffer.from(req.body.accessToken, 'base64').toString();
+        let newAuthCred = '';
+
+        // handling accessToken
+        if(req.body.email && req.body.password){
+            newAuthCred = `${req.body.email}:${req.body.password}`;
+            adminData.accessToken = Buffer.from(newAuthCred).toString('base64');
+        }else if(req.body.email){
+            oldPass = decode.split(":")[1];
+            newAuthCred = `${req.body.email}:${oldPass}`;
+            adminData.accessToken = Buffer.from(newAuthCred).toString('base64');
+        }else if(req.body.password){
+            oldEmail = decode.split(":")[0];
+            newAuthCred = `${oldEmail}:${req.body.password}`;
+            adminData.accessToken = Buffer.from(newAuthCred).toString('base64');
+        }
+
+        let updatedAdmin = await db.Admin.findOneAndUpdate({_id: req.params.admin_id}, adminData, {new: true});
         return res.status(200).json({
             updatedAdmin,
             success: {
