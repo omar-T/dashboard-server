59a0dc67daaa3530d9ec61b0b984d9c4a97a6d44
xotx <ot.themechanic95@gmail.com>
2020-01-22 10:46:33 +0300

    Update updateAdmin function. Add duplicate error handling.

--- handlers/admin.js
+++ handlers/admin.js
@@ -57,25 +57,7 @@ exports.deleteAdmin = async (req, res, next) => {
 // PUT - /api/admins/:admin_id
 exports.updateAdmin = async (req, res, next) => {
     try{
-        let adminData = req.body;
-        let decode = Buffer.from(req.body.accessToken, 'base64').toString();
-        let newAuthCred = '';
-        
-        // handling accessToken
-        if(req.body.email && req.body.password){
-            newAuthCred = `${req.body.email}:${req.body.password}`;
-            adminData.accessToken = Buffer.from(newAuthCred).toString('base64');
-        }else if(req.body.email){
-            oldPass = decode.split(":")[1];
-            newAuthCred = `${req.body.email}:${oldPass}`;
-            adminData.accessToken = Buffer.from(newAuthCred).toString('base64');
-        }else if(req.body.password){
-            oldEmail = decode.split(":")[0];
-            newAuthCred = `${oldEmail}:${req.body.password}`;
-            adminData.accessToken = Buffer.from(newAuthCred).toString('base64');
-        }
-
-        let updatedAdmin = await db.Admin.findOneAndUpdate({_id: req.params.admin_id}, adminData, {new: true});
+        let updatedAdmin = await db.Admin.findOneAndUpdate({_id: req.params.admin_id}, req.body, {new: true});
         return res.status(200).json({
             updatedAdmin,
             success: {
@@ -83,6 +65,12 @@ exports.updateAdmin = async (req, res, next) => {
             }
         });
     }catch(err){
-        return next(err);
+        if(err.code === 11000){
+            err.message = 'Sorry, that email is already taken!'
+        }
+        return next({
+            status: 400,
+            message: err.message
+        });
     }
 }
