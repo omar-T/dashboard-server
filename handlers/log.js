f5450a2c939592de78848299ab752af158f6c9ea
xotx <ot.themechanic95@gmail.com>
2020-01-26 21:15:23 +0300

    Add getActivityLastFiveDays route and handler.

--- handlers/log.js
+++ handlers/log.js
@@ -1,7 +1,8 @@
 const db = require('../models');
+const Moment = require('moment');
 
 // GET - /api/logs
-exports.getLogs = async function(req, res, next){
+exports.getLogs = async (req, res, next) => {
     try{
         let logs = await db.Log.find()
                             .sort({createdAt: 'desc'});
@@ -12,11 +13,120 @@ exports.getLogs = async function(req, res, next){
 }
 
 // POST - /api/logs
-exports.createLog = async function(req, res, next){
+exports.createLog = async (req, res, next) => {
     try{
         let log = await db.Log.create(req.body);
         return res.status(200).json(log);
     }catch(err){
         return next(err);
     }
+}
+
+// GET - /api/logs/mostActiveUsers
+exports.getMostActiveUsers = async (req, res, next) => {
+    try{
+        let logsData = await db.Log.aggregate([
+            {
+                $group: {
+                    _id: {userId: '$userId'},
+                    count: {$sum: 1}
+                }
+            },
+            {
+                $match: {
+                    count: {$gte: 2}
+                }
+            },
+            {
+                $sort: {count: -1}
+            },
+            {
+                $limit: 4
+            }
+        ])
+
+        let promiseData = logsData.map(log => {
+            return db.User.findById(log._id.userId, 'name surname')
+        })
+
+        Promise.all([...promiseData])
+            .then((values) => {
+                let usersData = logsData.map((log, index) => {
+                    return {
+                        ...values[index]._doc,
+                        count: log.count
+                    }
+                });
+                return res.status(200).json({
+                    usersData
+                });
+            })
+            .catch(err => {
+                return next(err);
+            });
+    }catch(err){
+        return next(err);
+    }
+}
+
+// GET - /api/logs/activityLastFiveDays
+exports.getActivityLastFiveDays = async (req, res, next) => {
+    try{
+        let labels = getDates(5, 'days');
+        let logs = await db.Log.aggregate([
+            {
+                $match: {
+                    createdAt: {
+                        $gte: labels[0]
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: {
+                        day: {$dayOfMonth: '$createdAt'}
+                    },
+                    count: {$sum: 1},
+                    date: {$min: '$createdAt'}
+                }
+            },
+            { $sort: {_id: 1} },
+            { $project: { date: "$date", count: 1, _id: 0} }
+        ]);
+
+        let fiveDaysLogs = [];
+        for(let i = 1; i < labels.length; i++){
+            let index = logs.findIndex(log => Moment(log.date).format('YYYY-MM-DD') === Moment(labels[i]).format('YYYY-MM-DD'));
+            if(index === -1){
+                fiveDaysLogs.push({
+                    count: 0,
+                    date: labels[i]
+                });
+            }else{
+                fiveDaysLogs.push({
+                    ...logs[index]
+                });
+            }
+        }
+        return res.status(200).json({
+            fiveDaysLogs
+        });
+    }catch(err){
+        return next(err);
+    }
+}
+
+// GET - /api/logs/activityLastFourWeeks
+exports.getActivityLastFourWeeks = async (req, res, next) => {
+    
+}
+
+const getDates = (diff, type) => {
+    let dateLabels = [];
+    const end = Moment();
+    let start = Moment().subtract(diff, type);
+    for(let m = start; m.diff(end, type) <= 0; m.add(1, type)){
+        dateLabels.push(new Date(m));
+    }
+    return dateLabels;
 }
--- routes/log.js
+++ routes/log.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const {getLogs} = require('../handlers/log');
+const {
+    getLogs, 
+    getMostActiveUsers, 
+    getActivityLastFiveDays,
+    getActivityLastFourWeeks
+} = require('../handlers/log');
 
 router.route('/')
     .get(getLogs);
 
+router.route('/mostActiveUsers')
+    .get(getMostActiveUsers);
+
+router.route('/activityLastFiveDays')
+    .get(getActivityLastFiveDays);
+
+router.route('/activityLastFourWeeks')
+    .get(getActivityLastFourWeeks);
 module.exports = router;
